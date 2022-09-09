d1278b0604ad2bd70c1e4ae628817c0e8df16f5b
RatiNabijashvili <ratinabija@gmail.com>
2022-09-09 14:39:33 +0400

    test commit

--- insure-landing-page/src/components/nav/nav.js
+++ insure-landing-page/src/components/nav/nav.js
@@ -6,13 +6,17 @@ const Nav = () => {
   return (
     <nav className={Styles.nav}>
       <div className={Styles.div}>
-        <img src='/images/logo.svg' alt='' className={Styles.img} />
-        <ul className={Styles.ul}>
-          <List name='HOW WE WORK' />
-          <List name='BLOG' />
-          <List name='ACCOUNT' />
-          <Button name='VIEW PLANS' className={Styles['nav-button']} />
-        </ul>
+        <div className={Styles.logo}>
+          <img src='/images/logo.svg' alt='' />
+        </div>
+        <div>
+          <ul className={Styles.ul}>
+            <List name='HOW WE WORK' />
+            <List name='BLOG' />
+            <List name='ACCOUNT' />
+            <Button name='VIEW PLANS' className={Styles['nav-button']} />
+          </ul>
+        </div>
       </div>
     </nav>
   )
